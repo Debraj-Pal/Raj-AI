7b85d303b40bddfc9920edea3931cc92dc05ebef
Debraj Pal <166520204+Debraj-Pal@users.noreply.github.com>
2024-05-08 00:38:19 +0530

    Add files via upload

---
+++ alarm.py
@@ -0,0 +1,44 @@
+import pyttsx3
+import os
+import datetime
+
+engine = pyttsx3.init()
+engine.setProperty('volume', 8.0)
+engine.setProperty('rate', 140)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+# convert text into speech
+def speak(text):
+    engine.say(text)
+
+    engine.runAndWait()
+
+extractedtime=open("Alarm.txt","rt")
+time = extractedtime.read()
+Time= str(time)
+
+extractedtime.close()
+
+deletetime= open("Alarm.txt","r+")
+deletetime.truncate(0)
+deletetime.close()
+
+def ring(time):
+    timeset = str(time)
+    timenow = timeset.replace("Jarvis","")
+    timenow = timenow.replace("set an alarm","")
+    timenow = timenow.replace(" and ",":")
+
+    AlarmTime=str(time)
+    print(AlarmTime)
+    while True:
+        currenttime = datetime.datetime.now().strftime("%H:%M:%S")
+        if currenttime ==AlarmTime:
+            speak("Alarm ringing, sir")
+            os.startfile("D:\\Software Back up\\Entertainment\\Songs\\Music\\Bones(PaglaSongs).mp3")
+        elif currenttime+"00:00:30"==AlarmTime:
+            exit()
+
+ring(time)
